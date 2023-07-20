d169d037fe4bacf2c61abfcc73855959d48d2ac8
luiscarlos <luiscarlos8pe@gmail.com>
2023-07-20 20:10:46 -0300

    Comentários de código

---
+++ comentarioDeCodigo.js
@@ -0,0 +1,18 @@
+/*
+console.log("Passo 01");
+console.log("Passo 01");
+Serve para comentar multiplas linhas
+*/
+
+
+/*
+ * console.log("Passo 01");
+ * console.log("Passo 01");
+ * Serve para comentar multiplas linhas
+*/
+
+
+//Pode-se usar aspas duplas ou simples a saida 
+//No finla de cada linha pode-se usar ou não ;
+console.log("Passo 01");
+console.log('Passo 01');//Usando aspas simples
