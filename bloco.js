301b2d797a2c087ea4a60ebfbe497d4ea8086c2f
luiscarlos <luiscarlos8pe@gmail.com>
2023-07-22 00:21:05 -0300

    Bloco de código

---
+++ bloco.js
@@ -0,0 +1,15 @@
+{
+    console.log("Passo 1");
+    console.log("Passo 2");
+}
+{
+    console.log("Passo 3");
+    console.log("Passo 4");
+
+}
+{
+    ;
+    ;
+    ssggfhdh; // quando ele encontra um problema ele nao execulta a ultima linha
+}
+console.log("FIM");
